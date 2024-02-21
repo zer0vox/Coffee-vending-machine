1d6e9577b6b97a7e1db8d5b83b9886909cab90c5
Sumip Chaudhary <84847274+zer0vox@users.noreply.github.com>
2024-02-21 21:34:24 +0545

    Add files via upload

---
+++ vendingmachine.vhd
@@ -0,0 +1,126 @@
+----------------------------------------------------------------------------------
+-- Company: Sumip Chaudhary
+-- Designer: Sumip Chaudhary
+-- 
+-- Create Date:    20:08:52 02/16/2024 
+-- Design Name: Coffee Vending Machine
+-- Module Name:    vendingmachine - Behavioral 
+-- Project Name: Vending Machine
+-- Target Devices: 
+-- Tool versions: 
+-- Description: A project for demonstration of a Coffee Vending Machine
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: embedded system -lab final project
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity vendingmachine is
+    Port ( clk : in  STD_LOGIC;
+           rstn : in  STD_LOGIC;
+           cashIn : in  STD_LOGIC_VECTOR (1 downto 0);
+           coffee : out  STD_LOGIC;
+           cashOut : out  STD_LOGIC_VECTOR (1 downto 0));
+end vendingmachine;
+
+architecture Behavioral of vendingmachine is
+type state_type is ( idle,--start state/reset
+ put_money, --waiting to enter money
+ in_1,in_3,in_6,in_5, --represent the current sum of money after returning change
+ change_1, --should return change of Rs 10
+ coffee_out --dispence coffee can.
+ ); --type of state machine.
+signal current_s,next_s: state_type; --current and next state declaration.
+
+begin
+--comb
+process(CLK,rstn)
+begin
+ if(rstn = '0') then
+ current_s <= idle; --defualt state is on RESET
+ elsif(clk'event and clk = '1') then
+ current_s <= next_s;
+ end if;
+end process;
+
+process(current_s,cashIn)
+begin
+case current_s is
+ when idle => 
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= put_money;
+ when put_money => --wait for money to be entered
+ if(cashIn = "00")then
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= put_money;
+ elsif(cashIn = "01")then --insert Rs 10
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= in_1;
+ elsif(cashIn = "10")then --insert Rs 20
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= coffee_out;
+ elsif(cashIn = "11")then --insert Rs 30
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= in_5;
+ end if;
+ ------------------------------------------------------
+ when in_1 => 
+ if(cashIn = "00") then--stay on the same state
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= in_1;
+ elsif(cashIn = "01") then--inserted another 10
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= coffee_out;
+ elsif(cashIn = "10") then--inserted another 20
+ coffee <= '0';
+ cashOut <= "00";
+ next_s <= in_3;
+ elsif(cashIn = "11") then
+ coffee <= '0';
+ cashOut <= "10";
+ next_s <= in_6;
+ end if;
+ ------------------------------------------------------
+ when in_3 =>
+ coffee <= '0';
+ cashOut <= "01";
+ next_s <= coffee_out;
+ ------------------------------------------------------
+ when in_6 =>
+ coffee <= '0';
+ cashOut <= "01";
+ next_s <= in_5;
+ ------------------------------------------------------
+ when in_5 => -- input = Rs 50
+ coffee <= '0';
+ cashOut <= "10";
+ next_s <= change_1;
+ ------------------------------------------------------
+ when change_1 => -- input = Rs 50
+ coffee <= '0';
+ cashOut <= "01";
+ next_s <= coffee_out;
+ ------------------------------------------------------
+ when coffee_out =>
+ coffee <= '1';
+ cashOut <= "00";
+ next_s <= put_money; 
+end case;
+end process;
+
+end Behavioral;
+
---
+++ vmtb.vhd
@@ -0,0 +1,120 @@
+
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   20:28:44 02/16/2024
+-- Design Name:   vendingmachine
+-- Module Name:   vmtb.vhd
+-- Project Name:  vendingmachine
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: vendingmachine
+--
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends 
+-- that these types always be used for the top-level I/O of a design in order 
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.all;
+USE ieee.numeric_std.ALL;
+
+ENTITY vmtb_vhd IS
+END vmtb_vhd;
+
+ARCHITECTURE behavior OF vmtb_vhd IS 
+	constant CLK_PERIOD: time  := 10 ns;
+	-- Component Declaration for the Unit Under Test (UUT)
+	COMPONENT vendingmachine
+	PORT(
+		clk : IN std_logic;
+		rstn : IN std_logic;
+		cashIn : IN std_logic_vector(1 downto 0);          
+		coffee : OUT std_logic;
+		cashOut : OUT std_logic_vector(1 downto 0)
+		);
+	END COMPONENT;
+
+	--Inputs
+	SIGNAL clk :  std_logic := '0';
+	SIGNAL rstn :  std_logic := '0';
+	SIGNAL cashIn :  std_logic_vector(1 downto 0) := (others=>'0');
+
+	--Outputs
+	SIGNAL coffee :  std_logic;
+	SIGNAL cashOut :  std_logic_vector(1 downto 0);
+
+BEGIN
+
+	-- Instantiate the Unit Under Test (UUT)
+	uut: vendingmachine PORT MAP(
+		clk => clk,
+		rstn => rstn,
+		cashIn => cashIn,
+		coffee => coffee,
+		cashOut => cashOut
+	);
+CLK_process: process
+    begin
+        while now < 200 ns loop
+            CLK <= '0';
+            wait for CLK_PERIOD/2;
+            CLK <= '1';
+            wait for CLK_PERIOD/2;
+        end loop;
+        wait;
+    end process CLK_process;
+	 
+	tb : PROCESS
+	BEGIN
+	 -- Reset
+        rstn <= '0';
+        wait for CLK_PERIOD;
+        rstn <= '1';
+
+        wait for CLK_PERIOD;
+
+        -- Scenario 1: No money
+        cashIn <= "00";
+        wait for CLK_PERIOD * 2;
+        
+        -- Scenario 2: Insert 10 followed by another 10
+        cashIn <= "01";
+        wait for CLK_PERIOD;
+        cashIn <= "01";
+        wait for CLK_PERIOD * 2;
+        
+        -- Scenario 3: Insert 10 followed by 20
+        cashIn <= "01";
+        wait for CLK_PERIOD;
+        cashIn <= "10";
+        wait for CLK_PERIOD * 2;
+        
+        -- Scenario 4: Insert 10 followed by 50
+        cashIn <= "01";
+        wait for CLK_PERIOD;
+        cashIn <= "11";
+        wait for CLK_PERIOD * 2;
+        
+        -- Scenario 5: Insert 20
+        cashIn <= "10";
+        wait for CLK_PERIOD * 2;
+
+        -- End simulation
+        wait;
+
+	END PROCESS;
+
+END;
